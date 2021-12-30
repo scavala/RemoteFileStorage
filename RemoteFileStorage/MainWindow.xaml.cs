16332b6eadaebf4844ed11d3abb2a71164d17417
sime <sime.cavala@gmail.com>
2021-12-30 01:37:11 +0100

    Add project files.

---
+++ RemoteFileStorage/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Azure.Storage.Blobs.Models;
+using Microsoft.Win32;
+using RemoteFileStorage.ViewModels;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RemoteFileStorage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ItemsViewModel itemsViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            itemsViewModel = new ItemsViewModel();
+            Init();
+        }
+
+        private void Init()
+        {
+            LbBlobs.ItemsSource = itemsViewModel.Items;
+            CbDirectories.ItemsSource = itemsViewModel.Directories;
+        }
+
+        private void LbBlobs_SelectionChanged(object sender, SelectionChangedEventArgs e) => DataContext = LbBlobs.SelectedItem as BlobItem;
+   
+
+        private async void BtnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files (JPEG,JPG,TIFF,PNG,SVG,GIF)| *.JPEG;*.PNG;*.JPG;*.TIFF;*.SVG;*.GIF"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string ext = Path.GetExtension(openFileDialog.FileName);
+                await itemsViewModel.UploadAsync(openFileDialog.FileName,ext);
+            }
+            CbDirectories.Text = itemsViewModel.Directory;
+        }
+
+        private async void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(LbBlobs.SelectedItem is BlobItem blobItem))
+            {
+                return;
+            }
+            await itemsViewModel.DeleteAsync(blobItem);
+            CbDirectories.Text = itemsViewModel.Directory;
+        }
+
+        private async void BtnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(LbBlobs.SelectedItem is BlobItem blobItem))
+            {
+                return;
+            }
+            var saveFileDialog = new SaveFileDialog()
+            {
+                FileName = blobItem.Name.Contains(ItemsViewModel.ForwardSlash) 
+                ? blobItem.Name.Substring(blobItem.Name.LastIndexOf(ItemsViewModel.ForwardSlash) + 1) 
+                : blobItem.Name
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                await itemsViewModel.DownloadAsync(blobItem, saveFileDialog.FileName);
+            }
+            CbDirectories.Text = itemsViewModel.Directory;
+        }
+
+ 
+        private void CbDirectories_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (itemsViewModel.Directories.Contains(CbDirectories.Text))
+            {
+                itemsViewModel.Directory = CbDirectories.Text;
+                CbDirectories.SelectedItem = itemsViewModel.Directory;
+
+            }
+        }
+
+      
+    }
+}
---
+++ RemoteFileStorage/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Storage.Blobs.Models;
+using RemoteFileStorage.Dao;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RemoteFileStorage.ViewModels
+{
+    class ItemsViewModel
+    {
+        public const string ForwardSlash = "/";
+        private string directory;
+        public ObservableCollection<BlobItem> Items { get; }
+        public ObservableCollection<string> Directories { get; }
+        public string Directory
+        {
+            get => directory;
+            set
+            {
+                directory = value;
+                Refresh();
+            }
+        }
+
+        public ItemsViewModel()
+        {
+            Items = new ObservableCollection<BlobItem>();
+            Directories = new ObservableCollection<string>();
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            Directories.Clear();
+            Items.Clear();
+            Repository.Container.GetBlobs().ToList().ForEach(item =>
+            {
+                if (item.Name.Contains(ForwardSlash))
+                {
+                    string directory = item.Name.Substring(0, item.Name.LastIndexOf(ForwardSlash));
+                    if (!Directories.Contains(directory))
+                    {
+                        Directories.Add(directory);
+                    }
+                }
+                if (string.IsNullOrEmpty(Directory) && !item.Name.Contains(ForwardSlash))
+                {
+                    Items.Add(item);
+                }
+                else if (!string.IsNullOrEmpty(Directory) && item.Name.Contains($"{Directory}{ForwardSlash}"))
+                {
+                    Items.Add(item);
+                }
+            });
+        }
+
+        public async Task UploadAsync(string path, string directory)
+        {
+            string filename = path.Substring(path.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+            filename = string.IsNullOrEmpty(directory?.Trim()) ? filename : $"{directory}{Path.DirectorySeparatorChar}{filename}";
+            using (var fs = File.OpenRead(path))
+            {
+                await Repository.Container.GetBlobClient(filename).UploadAsync(fs, true);
+            }
+            Refresh();
+        }
+
+        public async Task DeleteAsync(BlobItem blobItem)
+        {
+            await Repository.Container.GetBlobClient(blobItem.Name).DeleteAsync();
+            Refresh();
+
+        }
+
+        public async Task DownloadAsync(BlobItem blobItem, string filename)
+        {
+            using (var fs = File.OpenWrite(filename))
+            {
+                await Repository.Container.GetBlobClient(blobItem.Name).DownloadToAsync(fs);
+            }
+        }
+    }
+}
